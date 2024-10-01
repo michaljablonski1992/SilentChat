11396af44de8700ea4164ce0f8456f240e85d2a7
mjmj1992 <michal.jablonski1992@gmail.com>
2024-10-01 13:54:52 +0200

    loading logo fixes

--- components/shared/LoadingLogo.tsx
+++ components/shared/LoadingLogo.tsx
@@ -9,10 +9,12 @@ const LoadingLogo: React.FC = ({ size = 200 }: Props) => {
   return (
     <div className="h-full w-full flex justify-center items-center">
       <Image
+        id='loading-logo'
         src="/logo1.png"
         alt="Logo"
         width={size}
         height={size}
+        priority={true}
         className="animate-pulse duration-800"
       />
     </div>
